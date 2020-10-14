0a3792174de3c548ce39698136a2be8544f45d85
Yeasin Mahmud Jesan <ymjesanlemon@gmail.com>
2020-10-14 11:05:31 +0600

    mongoose connect

--- index.js
+++ index.js
@@ -1,21 +1,22 @@
 const PORT = process.env.PORT || 3000;
-const ex = require('express');
-
+const express = require('express');
 const mongoose = require('mongoose');
 require('dotenv/config');
 
-const app = ex();
-//Import ROUTES
+const app = express();
 
+//!Import ROUTES
 const postRoutes = require('./Routes/posts');
 
 app.use('/post',postRoutes);
 
-//Connect to DB
-// mongoose.connect(process.env.DB_CONNECTION,
-// {useUnifiedTopology : true},
-// () => console.log("Connected to DB."));
+
+//!Connect to DB
+mongoose.connect(process.env.DB_CONNECTION,
+{useUnifiedTopology : true},
+() => console.log("Connected to DB."));
 
 
-//listening 
+
+//!listening 
 app.listen(PORT,()=> {console.log("Server Started With Port : "+ PORT);});
